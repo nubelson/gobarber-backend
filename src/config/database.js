06c9a67ae7a67c0489a3f51f915280c95238c90b
nubelsondev <nubelsondev@hotmail.com>
2020-05-04 13:45:55 +0100

    DB config

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '4218',
+    database: 'gobarber',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
