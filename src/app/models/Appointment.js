9fa58771d489637129949a80727efbf27f4cd8fc
nubelsondev <nubelsondev@hotmail.com>
2020-05-12 07:32:55 +0100

    appointment Model

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,28 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+    static init(sequelize) {
+        super.init(
+            {
+                date: Sequelize.DATE,
+                canceled_at: Sequelize.DATE,
+            },
+            {
+                sequelize,
+            }
+        );
+
+        return this;
+    }
+
+    //! When a table has multiple relationships with another table, it is mandatory to pass the "as" property
+    static associate(models) {
+        this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+        this.belongsTo(models.User, {
+            foreignKey: 'provider_id',
+            as: 'provider',
+        });
+    }
+}
+
+export default Appointment;
