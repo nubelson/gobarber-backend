32f75b89eb636a17d009ae3d702f8e0611820aac
nubelsondev <nubelsondev@hotmail.com>
2020-05-12 08:01:44 +0100

    controller(appointment): create appointment

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import UserController from './app/controllers/userController';
 import SessionController from './app/controllers/sessionController';
 import FileController from './app/controllers/fileController';
 import ProviderController from './app/controllers/providerController';
+import AppointmentController from './app/controllers/appointmentController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +23,8 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+
+routes.post('/appointments', AppointmentController.store);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
